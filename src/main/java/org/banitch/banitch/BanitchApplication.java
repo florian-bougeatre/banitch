0bcefcff7e5585b99c164ad1432c3733867dc83d
florian-bougeatre <florian.bougeatre.pro@gmail.com>
2022-12-09 15:24:59 +0100

    First Commit

---
+++ src/main/java/org/banitch/banitch/BanitchApplication.java
@@ -0,0 +1,13 @@
+package org.banitch.banitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/banitch/banitch/BanitchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.banitch.banitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
